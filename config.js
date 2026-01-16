b3e8ecc4978331731059a5efc5fef8aab8c43ad8
msaddamboss1-cloud <msaddamboss1@gmail.com>
2026-01-16 10:34:37 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,23 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDRwrekZBZ31ACszDk2EJM_IqxRRwaGfCg",
-    authDomain: "bd-marketing-7389c.firebaseapp.com",
-    projectId: "bd-marketing-7389c",
-    storageBucket: "bd-marketing-7389c.appspot.com",
-    messagingSenderId: "788224536214",
-    appId: "1:788224536214:web:6e7464010834164f276633"
+  apiKey: "AIzaSyDRwrekZBZ31ACszDk2EJM_IqxRRwaGfCg",
+  authDomain: "bd-marketing-55a81.firebaseapp.com",
+  databaseURL: "https://bd-marketing-55a81-default-rtdb.firebaseio.com",
+  projectId: "bd-marketing-55a81",
+  storageBucket: "bd-marketing-55a81.firebasestorage.app",
+  messagingSenderId: "678696512628",
+  appId: "1:678696512628:web:edfc22a62f55dad41df521",
+  measurementId: "G-3CP5DC4H44"
 };
-firebase.initializeApp(firebaseConfig);
+
+// ফায়ারবেস কানেকশন চেক (পুরানো এবং নতুন ভার্সন যাতে মিসম্যাচ না হয়)
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
+
 const auth = firebase.auth();
 const db = firebase.firestore();
+const analytics = firebase.analytics ? firebase.analytics() : null;
+
+console.log("BD Marketing Database Connected Successfully!");
