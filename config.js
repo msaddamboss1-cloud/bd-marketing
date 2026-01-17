584a5615a7cac1f54bd50948fbb5752fe1a50265
msaddamboss1-cloud <msaddamboss1@gmail.com>
2026-01-18 03:41:49 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
-// Firebase Configuration for BD Marketing
-var firebaseConfig = {
+// Firebase Configuration
+const firebaseConfig = {
   apiKey: "AIzaSyDRwrekZBZ31ACszDk2EJM_IqxRRwaGfCg",
   authDomain: "bd-marketing-55a81.firebaseapp.com",
   databaseURL: "https://bd-marketing-55a81-default-rtdb.firebaseio.com",
   projectId: "bd-marketing-55a81",
   storageBucket: "bd-marketing-55a81.firebasestorage.app",
   messagingSenderId: "678696512628",
-  appId: "1:678696512628:web:edfc22a62f55dad41df521",
-  measurementId: "G-3CP5DC4H44"
+  appId: "1:678696512628:web:edfc22a62f55dad41df521"
 };
 
 // Initialize Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
-const auth = firebase.auth();
-const db = firebase.firestore();
 
+const auth = firebase.auth();
+const db = firebase.firestore(); // এটি নিশ্চিত করুন
