b6383284470060bc0d5d35559765cdab331a1f52
msaddamboss1-cloud <msaddamboss1@gmail.com>
2026-01-17 22:13:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDRwrekZBZ31ACszDk2EJM_IqxRRwaGfCg",
-  authDomain: "bd-marketing-55a81.firebaseapp.com",
-  projectId: "bd-marketing-55a81",
-  storageBucket: "bd-marketing-55a81.appspot.com",
-  messagingSenderId: "678696512628",
-  appId: "1:678696512628:web:edfc22a62f55dad41df521"
-};
 
-if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig); }
-const auth = firebase.auth();
-const db = firebase.firestore();
-const adminEmail = "msaddamboss1@gmail.com";
