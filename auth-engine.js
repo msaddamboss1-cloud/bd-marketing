a3a136e0b54c9551bbf3d7fdcb013bd34cbd5ad6
msaddamboss1-cloud <msaddamboss1@gmail.com>
2026-01-17 22:11:52 +0600

    Remove auth state change listener
    Remove redirect logic for unauthenticated users.

--- auth-engine.js
+++ auth-engine.js
@@ -1,5 +1,1 @@
-auth.onAuthStateChanged(user => {
-    if(!user && !window.location.pathname.includes("index.html") && !window.location.pathname.includes("register.html")) {
-        window.location.href = "index.html";
-    }
-});
+
