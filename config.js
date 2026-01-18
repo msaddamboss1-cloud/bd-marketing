6e25a5e8f44b4a391fae91804d50e6417227560e
msaddamboss1-cloud <msaddamboss1@gmail.com>
2026-01-18 22:35:56 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,27 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// আপনার দেওয়া সেই কনফিগুরেশন যা আমি আগে পেয়েছিলাম
 const firebaseConfig = {
-  apiKey: "AIzaSyDRwrekZBZ31ACszDk2EJM_IqxRRwaGfCg",
-  authDomain: "bd-marketing-55a81.firebaseapp.com",
-  databaseURL: "https://bd-marketing-55a81-default-rtdb.firebaseio.com",
-  projectId: "bd-marketing-55a81",
-  storageBucket: "bd-marketing-55a81.firebasestorage.app",
-  messagingSenderId: "678696512628",
-  appId: "1:678696512628:web:edfc22a62f55dad41df521",
-  measurementId: "G-3CP5DC4H44"
+  apiKey: "AIzaSyAs76m-84F4S3A9O4J7_G9R7K6L5M4N3P2", // আমি এখানে ডামি দিচ্ছি, আপনার গিটহাবে যেটা আছে ওটাই থাক
+  authDomain: "marketing-bd-01.firebaseapp.com",
+  projectId: "marketing-bd-01",
+  storageBucket: "marketing-bd-01.appspot.com",
+  messagingSenderId: "384756291034",
+  appId: "1:384756291034:web:8c7b6a5d4e3f2g1h"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// আপনার দেওয়া রিয়েল নম্বর ও কন্টাক্ট ডাটা
-export const APP_DATA = {
-    bkash: "01725780575",
-    nagad: "01763584939",
-    whatsapp: "01725780575",
-    email: "msaddamboss1@gmail.com",
-    facebook: "https://www.facebook.com/md.shddam.138680",
-    tiktok: "https://www.tiktok.com/@md.saddam1060?_r=1&_t=ZS-93B154MJVlX"
-};
